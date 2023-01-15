4dacd1706060bf3f82d74c4d4b9754d51373d636
Brianna Loc <jentink8@gmail.com>
2023-01-14 22:41:24 -0500

    working app with design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React, { useState, useRef } from 'react';
+
 import Header from './components/header.js'
 import SearchInput from './components/searchbar.js'
 import List from './components/trendingList.js'
 import Card from './components/card.js'
 
 function App() {
+  const [summary, setSummary] = useState('');
   return (
     
     <div className="App">
